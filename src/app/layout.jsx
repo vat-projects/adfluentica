973c1b86a7455c3e3065a42f94760e85bf1917d6
KonstantinRTS <k.developer@acceptance.ltd>
2024-12-30 11:48:23 +0200

    all files 04

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Urbanist, Roboto } from "next/font/google";
+import { Urbanist, Roboto, Space_Grotesk } from "next/font/google";
 import "@/styles/base.scss";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -22,13 +22,18 @@ const roboto = Roboto({
   display: "swap"
 });
 
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${urbanist.className} ${roboto.className}`}>
+      <body className={`${urbanist.className} ${roboto.className} ${spaceGrotesk.className}`}>
 
-        {/* <GoogleAnalytics gaId="" /> */}
+        <GoogleAnalytics gaId="G-3KC08Q4ZR7" />
         <PopupsProvider>
           <Preloader />
           <Header />
