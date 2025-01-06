e0df15f96083c5746e6a12a31f35ddeb0ebcee01
KonstantinRTS <k.developer@acceptance.ltd>
2025-01-06 17:18:02 +0200

    all files 24

--- src/app/api/emails/contact/route.js
+++ src/app/api/emails/contact/route.js
@@ -44,21 +44,20 @@ message: ${message}`,
       <table width="640" style="border-collapse: collapse; margin: 0 auto; font-style: sans-serif; border-right: 1px solid #222222; border-left: 1px solid #222222;">
     <thead>
         <tr>
-            <th style="background-image: url('https://adfluentica.com/images/letter-top.png'); background-size: cover;background-position: center center; background-repeat: no-repeat; height: 117px;"></th>
+            <th style="background-image: url('https://adfluentica.com/images/email-header.jpg'); background-size: cover;background-position: center center; background-repeat: no-repeat; height: 117px;"></th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td style="padding: 50px 40px; font-family: Roboto, sans-serif; color:#0A0A0A;">
                 <h2 style="text-align: left; font-size: 20px;">Dear ${yourName},</h2>
-                <p style="font-size: 16px; line-height: 19px;">Thank you for reaching out to us! We have received your message and appreciate you taking the time to contact Adfluentica.</p>
-                <p style="font-size: 16px; line-height: 19px;">Our team is currently reviewing your inquiry, and we will get back to you shortly. Please check your email for our response.</p>
-                <p style="font-size: 16px; line-height: 19px;">If you have any immediate questions or additional information to share, feel free to reply to this email.</p>
-                <p style="font-size: 16px; line-height: 19px;">Thank you for your patience, and we look forward to assisting you!</p>
+                <p style="font-size: 16px; line-height: 19px;">Thank you for reaching out to Adfluentica. We’ve received your message and will get back to you as soon as possible.</p>
+                <p style="font-size: 16px; line-height: 19px;">If your inquiry is urgent or requires immediate attention, feel free to reply to this email directly.</p>
+                <p style="font-size: 16px; line-height: 19px;">We appreciate your interest and look forward to assisting you!</p>
                 <p style="font-size: 16px; line-height: 19px; font-weight: 600;">
                     Best regards,
                     <br>
-                    Adfluentica Team
+                    The Adfluentica Team
                 </p>
             </td>
         </tr>
@@ -66,7 +65,7 @@ message: ${message}`,
     <tfoot>
         <tr>
             <td style="background-color: #333333; font-weight: 600; font-family: Roboto, sans-serif;padding: 30px 0;">
-                <a href="https://adfluentica.com/"><img src="https://adfluentica.com/images/letter-bottom.png" alt="Adfluentica"></a>
+                <a href="https://adfluentica.com/"><img src="https://adfluentica.com/images/email-footer.jpg" alt="Adfluentica"></a>
             </td>
         </tr>
     </tfoot>
